44ae3d991c2f7be5f7b7a1a6e7cb7aaadc1a9568
Bridget Kelly <bckelly197@gmail.com>
2022-07-12 00:09:35 -0400

    nothing showing when no city entered

--- src/App.js
+++ src/App.js
@@ -40,21 +40,25 @@ function App() {
             {data.weather ? <p>{data.weather[0].main} </p> : null}
           </div>
         </div>
+
+{data.name != undefined && 
         <div className="bottom"> 
-          <div className="feels">
-            {data.main ? <p> </p>}
-              <p className="bold" >65°F</p>
-              <p>Feels Like</p>
-          </div>
-          <div className="humidity">
-              <p className="bold" >20%</p>
-              <p>Humidity</p> 
-          </div>
-          <div className="wind">
-              <p className="bold" >12 MPH</p>
-              <p>Wind Speed</p>
-          </div>
+        <div className="feels">
+          {data.main ? <p className="bold"> {data.main.feels_like}°F </p> : null}
+            <p>Feels Like</p>
         </div>
+        <div className="humidity">
+          {data.main ? <p className="bold"> {data.main.humidity}% </p> : null}
+            <p>Humidity</p> 
+        </div>
+        <div className="wind">
+          {data.wind ? <p className="bold"> {data.wind.speed} MPH </p> : null}
+            <p>Wind Speed</p>
+        </div>
+        </div>
+}
+       
+
       </div>
 
     </div>
