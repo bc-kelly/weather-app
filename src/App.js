dccc40c3b8af50c6700325623ba1e2d4c2da4be6
Bridget Kelly <bckelly197@gmail.com>
2022-07-11 22:44:29 -0400

    api loaded and working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 function App() {
+
+  // const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=5787ee0fcc1ed6b74e1a41d4492c47ab'
+
+
   return (
     <div className="App">
       <h1> Hello </h1>
---
+++ src/weather.js
@@ -0,0 +1,47 @@
+// 20220711224128
+// https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=5787ee0fcc1ed6b74e1a41d4492c47ab
+
+// {
+//     "coord": {
+//       "lon": -96.7836,
+//       "lat": 32.7668
+//     },
+//     "weather": [
+//       {
+//         "id": 800,
+//         "main": "Clear",
+//         "description": "clear sky",
+//         "icon": "01n"
+//       }
+//     ],
+//     "base": "stations",
+//     "main": {
+//       "temp": 306.95,
+//       "feels_like": 307.76,
+//       "temp_min": 304.64,
+//       "temp_max": 308.59,
+//       "pressure": 1009,
+//       "humidity": 38
+//     },
+//     "visibility": 10000,
+//     "wind": {
+//       "speed": 2.57,
+//       "deg": 80
+//     },
+//     "clouds": {
+//       "all": 0
+//     },
+//     "dt": 1657593678,
+//     "sys": {
+//       "type": 2,
+//       "id": 2036480,
+//       "country": "US",
+//       "sunrise": 1657538849,
+//       "sunset": 1657589843
+//     },
+//     "timezone": -18000,
+//     "id": 4684904,
+//     "name": "Dallas",
+//     "cod": 200
+// }
+
