4751b8349fcfac7eadd949cdf2d0beae493729ce
Bridget Kelly <bckelly197@gmail.com>
2022-07-11 23:08:28 -0400

    styling page

--- src/App.js
+++ src/App.js
@@ -7,8 +7,33 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="app">
       <h1> Hello </h1>
+      <div className="container"> 
+        <div className="top">
+          <div className="location">
+            <p> Dallas </p>
+          </div>
+          <div className="temp">
+            <h1> 60°F </h1>
+          </div>
+          <div className="description">
+            <p> Clouds </p>
+          </div>
+        </div>
+        <div className="bottom"> 
+          <div className="feels">
+              <p> 65°F </p>
+          </div>
+          <div className="humidity">
+              <p> 20% </p>
+          </div>
+          <div className="wind">
+              12 MPH
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
